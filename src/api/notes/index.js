bc1e77dff18957f4ab4c67e66cd3f8a84b6a163a
vickypryma <vickymiya32@gmail.com>
2022-08-23 14:35:19 +0700

    restructure with hapi plugin

---
+++ src/api/notes/index.js
@@ -0,0 +1,12 @@
+const NotesHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, { service }) => {
+    const notesHandler = new NotesHandler(service);
+    const route = routes(notesHandler);
+    server.route(route);
+  },
+};
